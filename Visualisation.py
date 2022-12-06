b45f9c1cddc1785ed74da6d57cb7d48935334286
ArturSadykov2 <artursadykov31.10@gmail.com>
2022-12-06 20:00:25 +0300

    Test commit

--- Visualisation.py
+++ Visualisation.py
@@ -10,13 +10,13 @@ size = [1600, 900]
 
 screen = pg.display.set_mode(size)
 clock = pg.time.Clock()
-level_texture = pg.image.load('ball.png').convert_alpha()
-bg_texture = pg.image.load('image\main_backgroubd.png').convert_alpha()
+level_texture = pg.image.load('mask_lv2_without_background.png').convert_alpha()
+bg_texture = pg.image.load('bg_texture.jpg').convert_alpha()
 bg_texture = pg.transform.scale(bg_texture, size)
-level_texture = pg.transform.scale(level_texture, (500, 500))
+level_texture = pg.transform.scale(level_texture, size)
 bg_surface = pg.Surface(size, pg.SRCALPHA)
 bg_surface.blit(bg_texture, (0, 0))
-labirint_surface = pg.Surface((500, 500), pg.SRCALPHA)
+labirint_surface = pg.Surface(size, pg.SRCALPHA)
 labirint_surface.blit(level_texture, (0, 0))
 labirint_mask = pg.mask.from_surface(labirint_surface)
 
@@ -32,7 +32,7 @@ while running:
 
     screen.fill((255, 255, 255))
     screen.blit(bg_surface, (0, 0))
-    #screen.blit(labirint_surface, (0, 0))
+    screen.blit(labirint_surface, (0, 0))
     pg.display.flip()
     clock.tick(60)
 pg.quit()
