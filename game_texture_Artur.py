c30ff640a2f7d9899bfff8590c528588f5768fb4
ArturSadykov2 <artursadykov31.10@gmail.com>
2022-12-08 11:34:50 +0300

    Restructing files

--- game_texture_Artur.py
+++ game_texture_Artur.py
@@ -1,4 +1,18 @@
 import pygame as pg
 import os
+from game_texture_oleg import *
 
-lv1_light = pg.image.load(os.path.join("levels_image","lv1_light.png"))
+lv1_light = pg.image.load(os.path.join("levels_image", "lv1_light.png"))
+
+
+bg = pg.transform.scale(home_screen_png, size)
+home_surface = pg.Surface(size, pg.SRCALPHA)
+home_surface.blit(home, (0, 0))
+
+home = pg.transform.scale(home_screen_png, size)
+home_surface = pg.Surface(size, pg.SRCALPHA)
+home_surface.blit(home, (0, 0))
+
+home = pg.transform.scale(home_screen_png, size)
+home_surface = pg.Surface(size, pg.SRCALPHA)
+home_surface.blit(home, (0, 0))
